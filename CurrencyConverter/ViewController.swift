77693ec798824ae436a55033d741bb40ec796833
Jonathan Compton <jonathankcompton@gmail.com>
2018-02-14 14:07:00 -0800

    Initial Commit -- complete currency converter

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+
+    @IBOutlet weak var poundLabel: UILabel!
+    @IBOutlet weak var yenLabel: UILabel!
+    @IBOutlet weak var euroLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    let poundRate = 0.69
+    let yenRate = 113.94
+    let euroRate = 0.89
+    var dollarAmount = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        inputTextField.delegate = self
+        inputTextField.clearButtonMode = .always
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
+    @IBAction func convertCurrency(_ sender: Any) {
+        if let amount = Double(inputTextField.text!){
+            dollarAmount = amount
+        }
+        poundLabel.text = "\(dollarAmount * poundRate)"
+        yenLabel.text = "\(dollarAmount * yenRate)"
+        euroLabel.text = "\(dollarAmount * euroRate)"
+        dollarAmount = 0.0
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
